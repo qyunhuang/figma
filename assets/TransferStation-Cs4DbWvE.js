00932ffeafb6f889fdd2d1c38724868377ca88b0
spacey <3354909082@qq.com>
2025-02-28 23:58:29 +0800

    Updates

--- assets/TransferStation-Cs4DbWvE.js
+++
@@ -1,7 +0,0 @@
-/**
- * name: figma
- * version: v0.0.0
- * description: 
- * author: 
- */
-import{d as o,q as a,s as r,g as s,u as c,v as u,o as p}from"./index-CexLpgEO.js";const d=o({__name:"TransferStation",setup(_){const e=r(),t=a(()=>e.name?String(e.name)+new Date:String(e.path)+new Date);return(m,i)=>{const n=u("router-view");return p(),s(n,{key:c(t)})}}});export{d as default};
