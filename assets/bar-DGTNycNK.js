a38a090c989bd67e458ceba452189acdad1cbe74
spacey <3354909082@qq.com>
2025-03-04 16:50:59 +0800

    Updates

---
+++ assets/bar-DGTNycNK.js
@@ -0,0 +1,7 @@
+/**
+ * name: figma
+ * version: v0.0.0
+ * description: 
+ * author: 
+ */
+import{_ as n}from"./plugin-vueexport-helper-DlAUqK2U.js";import{c as r,a as e,j as s,k as a,D as c,o as l}from"./index-NhyQvNnl.js";const _={},d={class:"content"};function i(p,o){const t=c("router-link");return l(),r("div",d,[o[1]||(o[1]=e("p",null,"This is a children router.",-1)),s(t,{to:"/"},{default:a(()=>o[0]||(o[0]=[e("span",null,"Go Back",-1)])),_:1})])}const m=n(_,[["render",i],["__scopeId","data-v-94a1d099"]]);export{m as default};
