98f19ed594e974c5597673a08c4696052e85428e
spacey <3354909082@qq.com>
2025-03-05 17:33:30 +0800

    Updates

--- assets/TransferStation-ClbF6Ao_.js
+++
@@ -1,7 +0,0 @@
-/**
- * name: figma
- * version: v0.0.0
- * description: 
- * author: 
- */
-import{d as o,x as a,C as r,g as s,u as c,D as u,o as p}from"./index-NhyQvNnl.js";const d=o({__name:"TransferStation",setup(_){const e=r(),t=a(()=>e.name?String(e.name)+new Date:String(e.path)+new Date);return(m,i)=>{const n=u("router-view");return p(),s(n,{key:c(t)})}}});export{d as default};
